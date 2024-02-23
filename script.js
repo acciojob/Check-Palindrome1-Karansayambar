95f65f29832e5055868539f05985f1807652ce89
Karan Sayambar <86702398+Karansayambar@users.noreply.github.com>
2024-02-23 23:10:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 // complete the given function
 
-function palindrome(str){
-
+function palindrome(str) {
+    // Remove non-alphanumeric characters and convert to lowercase
+    const cleanStr = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    
+    // Compare the original string with its reversed version
+    return cleanStr === cleanStr.split('').reverse().join('');
 }
-module.exports = palindrome
+
+module.exports = palindrome;
+
